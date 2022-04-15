0fe4a18efec6909e2fffb1f715c4a898888f7ba8
1327wukai <wukai091@foxmail.com>
2022-04-15 09:57:02 +0800

    添加项目文件。

---
+++ Wukai-1327AllLabApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wukai_1327AllLabApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first Number:_ ");
+            double nl = Convert.ToDouble(Console.ReadLine()); 
+            Console.Write("Enter second Number: ");
+            double n2 = Convert.ToDouble(Console.ReadLine()); 
+            Console.Write("Enter operator: ");
+            string op = Console.ReadLine(); 
+            double result = 0;
+            bool valid = true;
+
+        }
+    }
+}
